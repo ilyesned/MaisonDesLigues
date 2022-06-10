2aa502ce9ece861dfcb82050b3642134466fad2c
Ilyes <ilyesned@hotmail.com>
2022-06-10 16:42:06 +0200

    bdd

--- src/connexion_bdd.inc.php
+++ src/connexion_bdd.inc.php
@@ -13,7 +13,7 @@
 
       try {
             $_pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            $bdd = new PDO('mysql:host=localhost;
+            $bdd = new PDO('mysql:host=172.190.1.52;
                             dbname=inedjai', 
                             'inedjai', 'azerty',
                             array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',$_pdo_options));
